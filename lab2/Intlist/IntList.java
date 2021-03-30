c0fff8bc694e0c66a07859c61a1a0fbe4d7921c5
Eli <lanxiao97@outlook.com>
2021-03-30 23:21:28 +0800

    completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
         IntList currList = A;
         while (currList.rest != null) {
             currList = currList.rest;
